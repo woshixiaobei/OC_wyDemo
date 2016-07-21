0df4ecd6709b31069248d81b7b1c9bef88de721b
wangmingzhu <wmz@qq.com>
2016-07-21 13:44:31 +0800

    添加文档目录

---
+++ 001-WY/001-WY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  001-WY
+//
+//  Created by 小贝 on 16/7/21.
+//  Copyright © 2016年 小贝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
